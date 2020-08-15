ac896ae3294fc48f75eb4d8cc797af6d2394f731
zoranaism <lazareviczorana@gmail.com>
2020-08-15 15:56:43 +0200

    Puppeteer

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require("puppeteer");
 const { generateText, checkAndGenerate } = require("./util");
 
 // UNIT TESTS
@@ -21,3 +22,23 @@ test("Shoukd generate a text valid output", () => {
   const text = checkAndGenerate("Zorana", 31);
   expect(text).toBe("Zorana (31 years old)");
 });
+
+// PUPPETEER TEST
+test("should create an element with text and correct class", async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    // slowMo: 80,
+    // args: ["--window-size=1920, 1080"],
+  });
+  const page = await browser.newPage();
+  await page.goto(
+    "file:///home/zorana/repos/testing-js-introduction/js-testing-introduction/index.html"
+  );
+  await page.click("input#name");
+  await page.type("input#name", "Anna");
+  await page.click("input#age");
+  await page.type("input#age", "28");
+  await page.click("#btnAddUser");
+  const finalText = await page.$eval(".user-item", el => el.textContent);
+  expect(finalText).toBe("Anna (28 years old)")
+}, 10000);
