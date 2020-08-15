820e9e357886c71a9ebd62619a0e396fceeffb24
zoranaism <lazareviczorana@gmail.com>
2020-08-15 15:30:28 +0200

    Integrations tests

--- util.test.js
+++ util.test.js
@@ -1,20 +1,23 @@
-const { generateText } = require("./util");
+const { generateText, checkAndGenerate } = require("./util");
 
 // UNIT TESTS
 
 test("should output name and age", () => {
   const text = generateText("Zorana", 31);
   expect(text).toBe("Zorana (31 years old)");
-//   const text2 = generateText("Anna", 28);
-//   expect(text2).toBe("Anna (28 years old)");
+  //   const text2 = generateText("Anna", 28);
+  //   expect(text2).toBe("Anna (28 years old)");
 });
 
 // confirmation that we dont have false positive and opposites
-// always good to have few examoples, but also the examples of the opposite answer 
+// always good to have few examoples, but also the examples of the opposite answer
 // test("should output data0less text", () => {
 //   const text = generateText("", null);
 //   expect(text).toBe(" (null years old)");
 // });
 
-
 // INTEGRATION TESTS
+test("Shoukd generate a text valid output", () => {
+  const text = checkAndGenerate("Zorana", 31);
+  expect(text).toBe("Zorana (31 years old)");
+});
